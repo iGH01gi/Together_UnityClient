31c10ae68afd70f4be662027bb5cc7d5e4eaf55d
igh <igh01pr@outlook.com>
2024-01-12 19:18:14 +0900

    [mid-save] PacketQueue 생성. 중간 저장
    //유니티메인쓰레드와 백그라운드 쓰레드(네트워크를 처리하는) 사이의 소통을 PacketQueue라는 통로를 이용해서 처리
    //메인 쓰레드에서는 Pop을 사용해서 처리

---
+++ Assets/Scripts/Server/Packet/PacketQueue.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//유니티메인쓰레드와 백그라운드 쓰레드(네트워크를 처리하는) 사이의 소통을 PacketQueue라는 통로를 이용해서 처리
+//메인 쓰레드에서는 Pop을 사용해서 처리
+public class PacketQueue
+{
+    public static PacketQueue Instance { get; } = new PacketQueue();
+    
+    Queue<IPacket> _packetQueue = new Queue<IPacket>();
+    object _lock = new object();
+
+    public void Push(IPacket packet)
+    {
+        lock (_lock)
+        {
+            _packetQueue.Enqueue(packet);
+        }
+    }
+        
+    public IPacket Pop()
+    {
+        lock (_lock)
+        {
+            if (_packetQueue.Count == 0)
+                return null;
+            
+            _packetQueue.Dequeue();
+        }
+    }
+}
