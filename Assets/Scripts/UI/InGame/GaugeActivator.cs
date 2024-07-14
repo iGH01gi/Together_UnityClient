f157bd5a1a148512c20306de71341b9712ea70e4
iGH01gi <iGH01pr@outlook.com>
2024-07-14 18:35:12 +0900

    게이지 이상하던 오류 해결(gaugeActivator의 상속 으로 인한 문제)

--- Assets/Scripts/UI/InGame/GaugeActivator.cs
+++ Assets/Scripts/UI/InGame/GaugeActivator.cs
@@ -2,23 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GaugeActivator : ClientGauge
+public class GaugeActivator : MonoBehaviour
 {
     //client 자체 카운트 다운
-
-    private void Awake()
-    {
-        _gaugeActivator = this;
-    }
-
 
     // Update is called once per frame
     void Update()
     {
-        float old = GetMyGauge();
-        DecreaseAllGaugeAuto();
-        Debug.Log(Managers.Player._myDediPlayer.GetComponent<MyDediPlayer>()._gauge);
-        float cur = GetMyGauge();
-        Managers.UI.GetComponentInSceneUI<InGameUI>().SetCurrentGauge(cur);
+        Managers.Game._clientGauge.DecreaseAllGaugeAuto();
     }
 }
