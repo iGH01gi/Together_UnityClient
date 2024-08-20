d6e6744a3233fbed573ee3cda28f2d5060fb2ce4
mjk8 <mjktt8@gmail.com>
2024-08-21 02:01:24 +0900

    Factories and onhold before final test

--- Assets/Scripts/Item/Factory/InvisibleFactory.cs
+++ Assets/Scripts/Item/Factory/InvisibleFactory.cs
@@ -7,7 +7,7 @@ public class InvisibleFactory : ItemFactory
     public float InvisibleSeconds { get; set; }
     
     public InvisibleFactory(int id, int price, string englishName, string koreanName, string englishDescription,
-        string koreanDescription, float dashDistance, float invisibleSeconds)
+        string koreanDescription, float invisibleSeconds)
     {
         base.FactoryInit(id, price, englishName, koreanName, englishDescription, koreanDescription);
         InvisibleSeconds = invisibleSeconds;
